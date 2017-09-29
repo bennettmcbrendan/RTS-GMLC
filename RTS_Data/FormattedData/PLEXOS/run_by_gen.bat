50ea1fac682031b47957492a95a74941190acafd
McBennett <brendan.mcbennett@gmail.com>
2017-09-29 11:46:30 -0600

    add nodal load, unserved and dump energy to reports

--- RTS_Data/FormattedData/PLEXOS/run_by_gen.bat
+++
@@ -1,8 +0,0 @@
-cd "C:\Program Files (x86)\Energy Exemplar\PLEXOS 7.3\"
-.\PLEXOS64.EXE \\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\\RTS-GMLC-GeoDecomp_bygen2.xml \m "DAY_AHEAD_A" \n
-start "" "C:\Program Files\R\R-3.3.2\bin\Rscript.exe" "\\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\Intermediate_scripts\intermediate_reserves.R"
-.\PLEXOS64.EXE \\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\\RTS-GMLC-GeoDecomp_bygen2.xml \m "DAY_AHEAD" \n
-.\PLEXOS64.EXE \\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\\RTS-GMLC-GeoDecomp_bygen2.xml \m "DAY_AHEAD_B1" \n
-.\PLEXOS64.EXE \\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\\RTS-GMLC-GeoDecomp_bygen2.xml \m "DAY_AHEAD_B2" \n
-.\PLEXOS64.EXE \\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\\RTS-GMLC-GeoDecomp_bygen2.xml \m "DAY_AHEAD_B3" \n
-.\PLEXOS64.EXE \\plexossql\Data\bmcbenne\RTS-GMLC-geodecomp\RTS-GMLC\RTS_Data\FormattedData\PLEXOS\\RTS-GMLC-GeoDecomp_bygen2.xml \m "REAL_TIME_C" \n
