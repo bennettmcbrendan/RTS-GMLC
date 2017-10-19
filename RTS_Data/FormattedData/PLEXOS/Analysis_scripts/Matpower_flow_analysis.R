4b7b497a2dcb29ce26eaf74a8d807c9f86f0a050
ake04 <ake04>
2017-10-19 12:53:06 -0600

    add ERGIS flow-price comparison script

---
+++ RTS_Data/FormattedData/PLEXOS/Analysis_scripts/Matpower_flow_analysis.R
@@ -0,0 +1,162 @@
+
+pacman::p_load(rplexos, RSQLite, magrittr, dplyr, lubridate, 
+               rmarkdown, scales, cowplot, data.table, fasttime,
+               Hmisc, gridExtra, rgdal, ggmap, Cairo, rgeos, maptools, 
+               lubridate, plyr, gdata, stringr, tidyr, dtplyr, knitr,grid)
+
+wd = dirname(parent.frame(2)$ofile)
+setwd(wd)
+source("source_scripts/plot_parameters.R")
+
+# ----------------------------------------------------------------------- |
+# Inputs ----
+# ----------------------------------------------------------------------- |
+
+interfaces.to.plot <- c("PJM - MISO","PJM - NYISO","NYISO - ISO-NE")
+color.code = c("PJM - MISO" = 'navyblue',"PJM - NYISO" = 'forestgreen',"NYISO - ISO-NE" = 'firebrick')
+
+solutions.dir <- "//nrelqnap01d/PLEXOS/Projects/ERGIS/ERGIS Solutions/c_RT_loVG"
+
+# map of ERGIS to desired region names
+ERGIS.regions <- file.path("//plexossql/Data/bmcbenne/RTS-GMLC-geodecomp/RTS-GMLC",
+                           "RTS_Data/FormattedData/PLEXOS/ERGIS_regions.csv")
+
+# interface map
+ERGIS.interfaces <- file.path("//plexossql/Data/bmcbenne/RTS-GMLC-geodecomp/RTS-GMLC",
+                           "RTS_Data/FormattedData/PLEXOS/ERGIS_interfaces.csv")
+
+# ----------------------------------------------------------------------- |
+# Setup queries ----
+# ----------------------------------------------------------------------- |
+
+setwd(solutions.dir)
+
+# solution dbs & scenario.names
+ERGIS.solutions = list.files()[grepl("-rplexos.db",list.files())]
+scenario.names = as.character(seq(1,length(ERGIS.solutions),1))
+
+ERGIS.regions = fread(ERGIS.regions)
+
+ERGIS.interfaces = fread(ERGIS.interfaces)
+interfaces.to.plot = ERGIS.interfaces[,Interface]
+ISO.from = ERGIS.interfaces[,ISO.From]
+ISO.To = ERGIS.interfaces[,ISO.To]
+color.code = ERGIS.interfaces[,color]
+names(color.code) = interfaces.to.plot
+
+# ----------------------------------------------------------------------- |
+# Queries ----
+# ----------------------------------------------------------------------- |
+
+# 1. interval region price
+
+interval.region.price <- data.table()
+for(i in 1:length(scenario.names)){
+  print(i)
+  interval.region.price <- rbind(interval.region.price,
+                                expand_time(data.table(tbl(src_sqlite(ERGIS.solutions[i]), 
+                                sql("SELECT key, name, category, time_from, time_to, value, property 
+                                FROM Region_Price 
+                                WHERE collection IS 'Region' AND 
+                                property IS 'Price' AND
+                                phase_id IS 4")) %>% 
+                                dplyr::mutate(scenario = scenario.names[i]) %>% 
+                                collect(n = Inf)),ERGIS.solutions[i]))
+}
+
+# time partitions
+int.region.price = copy(interval.region.price)
+int.region.price[,min.scenario:=min(scenario),by = c('name','time')]
+int.region.price = int.region.price[scenario == min.scenario]
+
+# 2. interval Region.Region net interchange
+
+interval.region.NI <- data.table()
+for(i in 1:length(scenario.names)){
+  print(i)
+  interval.region.NI <- rbind(interval.region.NI,
+                                expand_time(data.table(tbl(src_sqlite(ERGIS.solutions[i]), 
+                                sql("SELECT key, name, parent, category, time_from, time_to, value, property 
+                                FROM RegionRegions_NetInterchange 
+                                WHERE collection IS 'Region.Regions' AND 
+                                property IS 'Net Interchange' AND
+                                phase_id IS 4")) %>% 
+                                dplyr::mutate(scenario = scenario.names[i]) %>% 
+                                collect(n = Inf)),ERGIS.solutions[i]))
+}
+
+# time partitions
+int.region.NI = copy(interval.region.NI)
+int.region.NI[,min.scenario:=min(scenario),by = c('name','time')]
+int.region.NI = int.region.NI[scenario == min.scenario]
+
+
+# ----------------------------------------------------------------------- |
+# Calculations ----
+# ----------------------------------------------------------------------- |
+
+# prices
+
+int.region.price = merge(int.region.price[,.(Region = name,time,value)],
+                         ERGIS.regions,
+                         by = 'Region')
+
+# average price by ISO
+int.region.price = int.region.price[,lapply(.SD,mean),by = c('ISO','time'),.SDcols = 'value']
+
+int.region.price = data.table(dcast(int.region.price,time ~ ISO,value.var = 'value'))
+
+
+for(j in seq(length(interfaces.to.plot))){
+
+  int.region.price[,get(interfaces.to.plot[j]):=get(ISO.From[j]) - get(ISO.To[j])]
+  
+}
+
+int.region.price = int.region.price[,.SD,.SDcols = c(interfaces.to.plot,time)]
+int.region.price[,var:='Price']
+
+# flows
+
+setnames(int.region.NI,c('parent','name'),c('Region.From','Region.To'))
+int.region.NI = merge(int.region.NI,
+                      ERGIS.regions[,.(Region.From = Region,ISO.From = ISO)],
+                      by = 'Region.From')
+
+int.region.NI = merge(int.region.NI,
+                      ERGIS.regions[,.(Region.To = Region,ISO.To = ISO)],
+                      by = 'Region.To')
+
+int.region.NI = int.region.NI[,lapply(.SD,sum),by = c('ISO.From','ISO.To','time'),.SDcols = 'value']
+
+int.region.NI[,Interface:=paste0(ISO.From," - ",ISO.To)]
+int.region.NI = data.table(dcast(int.region.NI,time ~ Interface,value.var = 'value'))
+int.region.NI[,var:= "Interchange"]
+
+int.region.NI = int.region.NI[,.SD,.SDcols = names(int.region.price)]
+
+# combine
+
+int.comparison = rbind(int.region.NI,int.region.price)
+
+int.comparison = data.table(melt(int.comparison, id.vars = c("time","var"),
+                                 measure.vars = interfaces.to.plot))
+
+setnames(int.comparison,c('var','variable'),c('variable','Interface'))
+
+int.comparison = data.table(dcast(int.comparison,time + Interface ~ variable,value.var = 'value'))
+
+# ----------------------------------------------------------------------- |
+# Plot ----
+# ----------------------------------------------------------------------- |
+
+p <- ggplot() + geom_point(data = int.comparison,aes(x = Interchange,y = Price,color = Interface),size = 0.3) + 
+  scale_color_manual(values = color.code) + 
+  facet_grid(.~Interface) + plot_theme + labs(x = 'Interchange (MW)',y = 'LMP difference (USD)') +
+  theme(legend.position = 'none')
+
+setwd(wd)
+ggsave('ERGIS_efficiencies.png',p,height = 4,width = 6.5)
+
+
+
---
+++ RTS_Data/FormattedData/PLEXOS/Analysis_scripts/source_scripts/plot_parameters.R
@@ -0,0 +1,68 @@
+
+# ----------------------------------------------------------------------- |
+# functions ----
+# ----------------------------------------------------------------------- |
+
+# A. Expand time function
+expand_time = function(data, db.path, phaseid = 4, look.ahead = 0) {
+  setnames(data, "time_from", "time")
+  data$time = ymd_hms(data$time)
+  datadt = data.table(data, key = "key,time")
+  
+  # get time from database being queried
+  timedt = data.table(tbl(src_sqlite(db.path), sql("SELECT * FROM time")) %>% 
+                        filter(phase_id == phaseid) %>% collect())
+  timedt$time = ymd_hms(timedt$time)  #R format time
+  
+  # drop extra look ahead days
+  keep.days = unique(timedt[,.(day = floor_date(time, unit = "day"))])
+  keep.days = keep.days[1:(nrow(keep.days) - look.ahead)]
+  timedt = timedt[floor_date(time, unit = "day") %in% keep.days$day,]
+  
+  # Expand data
+  cj2 = CJ(key = unique(datadt$key), time = timedt$time)
+  cj3 = datadt[cj2, roll = TRUE]
+  
+  # Restore timezone (UTC)
+  attributes(cj3$time) = attributes(timedt$time)
+  cj3 = cj3[, `:=`(time_to, NULL)]
+  
+  cj3
+}
+
+# B. Error handling
+error_handler <- function(query){
+  result <- tryCatch(query,error = function(cond) { return('ERROR') } )
+  if(class(result)[1] != "character"){
+    if(nrow(result) == 0) {result <- 'ERROR'}
+  }
+  return(result)
+}
+
+# ----------------------------------------------------------------------- |
+# plot theme ----
+# ----------------------------------------------------------------------- |
+
+# set ggplot theme - make background nicer
+theme_set(theme_bw())
+
+# size of text in plots
+large.text.size <- 10.5
+small.text.size <- 9.1666
+text.plot = 11
+
+# plot theme
+plot_theme <- 
+  theme(legend.key = element_rect(color = "grey80", size = 0.8), 
+        legend.key.size = grid::unit(1.0, "lines"),
+        legend.text = element_text(size = small.text.size), 
+        legend.title = element_blank(), 
+        axis.text = element_text(size = small.text.size), 
+        axis.text.x = element_text(size = small.text.size),
+        axis.title = element_text(size = large.text.size, face = "bold"),
+        axis.title.x= element_text(size=large.text.size, vjust = 1.2, face = "bold"),
+        axis.title.y = element_text(size=large.text.size, vjust = 1.2, face = "bold"),
+        strip.text = element_text(size=small.text.size),
+        panel.spacing = unit(0.5, "lines"))
+
+
